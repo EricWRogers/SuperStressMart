dd7e1feb22fc7610bb1e131c8f91a52cd36b985a
Melisa Johnson <melisa@asteri-networks.com>
2019-01-26 05:30:58 -0600

    Adds function to the stress meter and adds shopping list

---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Image stressMeter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SubtractStressBar(float percentage)
+    {
+        stressMeter.fillAmount -= percentage;
+    }
+    public void AddStressBar(float percentage)
+    {
+        stressMeter.fillAmount += percentage;
+    }
+}
---
+++ Assets/Scripts/UI/UiEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UI))]
+public class UiEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        UI ui = (UI)target;
+        if (GUILayout.Button("-"))
+        {
+            ui.SubtractStressBar(0.2f);
+        }
+        if (GUILayout.Button("+"))
+        {
+            ui.AddStressBar(0.3f);
+        }
+    }
+}
