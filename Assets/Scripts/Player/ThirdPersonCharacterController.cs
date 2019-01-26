5a79e7998c5885c87f6ac4ba4009bb09b3f36f6f
Eric <yerick147083@gmail.com>
2019-01-25 23:05:43 -0600

    Taking out  StandardAssets

---
+++ Assets/Scripts/Player/ThirdPersonCharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayerMovement()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0.0f,Input.GetAxis("Vertical")) * Speed * Time.deltaTime, Space.Self);
+    }
+}
