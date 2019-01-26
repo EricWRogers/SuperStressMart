d17dd47fe056ae0dc56c196eb619f1fef2d345d0
Melisa Johnson <melisa@asteri-networks.com>
2019-01-26 14:37:51 -0600

    Adds Sound Enum

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -2,17 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SoundEvents
+{
+    StartingGame,
+    NPCTalking,
+    NPCTalkingToEmily,
+    PickingUpItem,
+    HidesInBathroom,
+    PathBlocked,
+    AnxietyLax,
+    NPCStandingBetween,
+    FireAlarm,
+    Success,
+    PassesOut,
+    ArgumentBreaksOut,
+    CardDeclined,
+    HighAnxiety,
+    CheckingOut,
+    SeeingEx,
+    ShelfFalls
+}
+
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //private SoundEvents soundEvents = new SoundEvents();
+   // [SerializeField]
+    //private AudioSource voiceSource;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void SoundsEventTrigger(SoundEvents soundEvents)
+    {
+        switch (soundEvents)
+        {
+            case SoundEvents.StartingGame:
+                break;
+            case SoundEvents.NPCTalking:
+                break;
+            case SoundEvents.NPCTalkingToEmily:
+                break;
+            case SoundEvents.PickingUpItem:
+                break;
+            case SoundEvents.HidesInBathroom:
+                break;
+            case SoundEvents.PathBlocked:
+                break;
+            case SoundEvents.AnxietyLax:
+                break;
+            case SoundEvents.NPCStandingBetween:
+                break;
+            case SoundEvents.FireAlarm:
+                break;
+            case SoundEvents.Success:
+                break;
+            case SoundEvents.PassesOut:
+                break;
+            case SoundEvents.ArgumentBreaksOut:
+                break;
+            case SoundEvents.CardDeclined:
+                break;
+            case SoundEvents.HighAnxiety:
+                break;
+            case SoundEvents.CheckingOut:
+                break;
+            case SoundEvents.SeeingEx:
+                break;
+            case SoundEvents.ShelfFalls:
+                break;
+        }
     }
 }
