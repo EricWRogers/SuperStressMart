a8b07bc1806046259542e39a6b350ee794edbc15
Eric <yerick147083@gmail.com>
2019-01-26 01:10:20 -0600

    Player scene update

--- Assets/Scripts/Player/ThirdPersonCameraController.cs
+++ Assets/Scripts/Player/ThirdPersonCameraController.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonPlayerCo : MonoBehaviour
+public class ThirdPersonCameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float RotationSpeed = 2.0f;
+    public Transform Target, Player;
+
+    private float mouseX, mouseY;
+
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CanControl();
+    }
+
+    void CanControl()
+    {
+        mouseX += (Input.GetAxis("Mouse X") * RotationSpeed) * Time.deltaTime;
+        mouseY -= (Input.GetAxis("Mouse Y") * RotationSpeed) * Time.deltaTime;
+
+        mouseY = Mathf.Clamp(mouseY, 20.0f, 60.0f);
+        // mouseX = Mathf.Clamp(mouseX, 0.0f, 360.0f);
+
+        // Debug.Log("MouseY: " + mouseY + " MouseX: " + mouseX);
+
+        //transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0.0f);
+        Player.rotation = Quaternion.Euler(0.0f, mouseX, 0.0f);
+        //transform.LookAt(Target);
     }
 }
