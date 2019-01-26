8be343a3530829fd4fb7f4e7da2bd0b31e262924
Melisa Johnson <melisa@asteri-networks.com>
2019-01-26 09:54:29 -0600

    Adds button functions and back buttons

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -5,13 +5,27 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    //Start Menu
+    public GameObject startMenu;
+
+    //Options
+    public GameObject optionsMenu;
+
+    //Credits
+    public GameObject creditsMenu;
+
+    //hud
+    public GameObject hud;
     public Image stressMeter;
     public GameObject shoppingListObject;
     private int open = 0;
 
     void Start()
     {
-        
+        startMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        hud.SetActive(false);
     }
 
     void Update()
@@ -42,5 +56,31 @@ public class UI : MonoBehaviour
             shoppingListObject.SetActive(false);
             open = 0;
         }
+    }
+
+    public void StartButton()
+    {
+        startMenu.SetActive(false);
+        hud.SetActive(true);
+    }
+    public void OptionsButton()
+    {
+        startMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void OptionsBackButton()
+    {
+        startMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+    public void CreditsButton()
+    {
+        startMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+    public void CreditsBackButton()
+    {
+        startMenu.SetActive(true);
+        creditsMenu.SetActive(false);
     }
 }
